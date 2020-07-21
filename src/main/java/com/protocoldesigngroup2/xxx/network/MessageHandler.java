1e12bb2da34fa61267056dd62c06189bfe92a903
Lennard <lennard-heute@web.de>
2020-07-21 15:20:58 +0200

    Added network part

--- src/main/java/com/protocoldesigngroup2/xxx/network/MessageHandler.java
+++ src/main/java/com/protocoldesigngroup2/xxx/network/MessageHandler.java
@@ -2,6 +2,7 @@ package com.protocoldesigngroup2.xxx.network;
 
 import com.protocoldesigngroup2.xxx.messages.Message;
 
+
 public interface MessageHandler {
     public void handleMessage(Message message, Endpoint endpoint);
 }
