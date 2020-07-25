820da6dcce90cb5966e7100d5aad8e09d4ce3c95
Lennard <lennard-heute@web.de>
2020-07-25 13:01:48 +0200

    Added serialVersionUID for WrongIdException

--- src/main/java/com/protocoldesigngroup2/xxx/messages/WrongIdException.java
+++ src/main/java/com/protocoldesigngroup2/xxx/messages/WrongIdException.java
@@ -2,6 +2,8 @@ package com.protocoldesigngroup2.xxx.messages;
 
 
 public class WrongIdException extends Exception {
+    private static final long serialVersionUID = -7402177213564454996L;
+
     public WrongIdException(String message) {
         super(message);
     }
