a93e439af6ab402a2fc6d2c4e725b3ad504672fe
Richard Nai <richardnai6@gmail.com>
2020-08-13 22:46:20 +0200

    Hopefully good

--- src/main/java/com/protocoldesigngroup2/xxx/client/Client.java
+++ src/main/java/com/protocoldesigngroup2/xxx/client/Client.java
@@ -280,6 +280,9 @@ public class Client {
                     numberOfChunks++;
                 } else {
                     if (numberOfChunks > 0) {
+                        if (numberOfChunks == 256) {
+                            numberOfChunks = 255;
+                        }
                         utils.printDebug("Add resend entry:\tOffset: " + resendOffset + "\tNumber of Chunks: " + numberOfChunks);
                         // If there is a chunk for the offset build a resend entry for the preceding sequence of unreceived offsets
                         resendEntries.add(new ResendEntry(entry.getKey(), resendOffset, numberOfChunks));
